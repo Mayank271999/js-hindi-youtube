eeceed9dbbf9dc80a03149ee490e7b39d48b508c
Mayank271999 <81458461+Mayank271999@users.noreply.github.com>
2025-07-19 16:45:09 +0000

    adding variables

---
+++ 02_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553  // cant be changed 
+let accountEmail = "mayankdeopamd@gmail.com" //  
+var accountPassword = "12345"/*prefer not to use var because of issue in block scope and fucntional
+scope
+
+*/
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 2
+accountEmail = " hc@hc.cm"
+accountPassword = "212121"
+accountCity = "noida"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
